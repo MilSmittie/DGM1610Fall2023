ac115e8f22e6339067f4ec94d0caf40d493cf9b7
MilSmittie <122853314+MilSmittie@users.noreply.github.com>
2023-09-05 22:22:03 -0600

    Updated HelloWorld

---
+++ HelloWorld.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        //Change "HW" to "Hello World"
+        Console.WriteLine("Hello");
+        Console.WriteLine("World");
+    }    
+}
