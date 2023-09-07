d45d5df292a2257606228256d4094dfb27863fc7
MilSmittie <122853314+MilSmittie@users.noreply.github.com>
2023-09-06 22:39:29 -0600

    Working on Unit1 Challenge

---
+++ Unit1/Unit1aChallenge/ClassExampleEdits.cs
@@ -0,0 +1,22 @@
+public class ClassExample
+{
+    
+}
+
+public class ClassWithVariables
+{
+    public int number = 2;
+    public string userName = "John Cena";
+    public char letter = 'D'
+    public float inches = 7.69f;
+}
+
+public class ClassWithFunctions
+{
+    public void RunFunction()
+    {
+        Console.WriteLine("JOHN CENA");
+        Console.WriteLine("do dodo dooooo");
+        Console.WriteLine(letter + " = " + inches);
+    }
+}   
