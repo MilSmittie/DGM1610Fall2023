12f8d2c869f1e81013131e95a11684b546064867
MilSmittie <122853314+MilSmittie@users.noreply.github.com>
2023-09-07 23:40:17 -0600

    Fixing Errors

--- Unit1/HeroClass.cs
+++ Unit1/HeroClass.cs
@@ -4,11 +4,13 @@ public class Program
 {
 	public Hero heroOne;
 	public Hero heroTwo;
+	public Hero heroThree;
 	
 	public void Main()
 	{
 		heroOne = new Hero();
 		heroTwo = new Hero();
+		heroThree = new Hero();
 		
 		//hero's health and powerlevel levels
 		heroOne.health = 1;
@@ -16,12 +18,17 @@ public class Program
 		
 		heroTwo.health = 3;
 		heroTwo.powerLevel = 1;
+
+		heroThree.health = 2;
+		heroThree.powerLevel = 4;
 		
 		//changing statistics
 		heroOne.health --;
 		heroOne.powerLevel --;
         heroTwo.powerLevel ++;
 		heroTwo.health ++;
+		heroThree.health --;
+		heroThree.powerLevel ++;
 		
 		//if hero dies itll say
 		if (heroOne.health <= 0) 
@@ -41,6 +48,14 @@ public class Program
 		{
 			Console.WriteLine("YOU KILLED LEBRON JAMES.");
 		}
+		if (heroThree.health <= 0)
+		{
+			Console.WriteLine("Dani Devito lives another day!");
+		}
+		else
+		{
+			Console.WriteLine("YOU KILLED DANI DEVITO.");
+		}
 		
 		//writing out hero's health and power amounts
 		Console.WriteLine(heroOne.heroOneName + "'s health is " + heroOne.health);
@@ -48,6 +63,9 @@ public class Program
 		
 		Console.WriteLine(heroTwo.heroTwoName + "'s health is " + heroTwo.health);
 		Console.WriteLine(heroTwo.heroTwoName + "'s power level is " + heroTwo.powerLevel);
+
+		Console.WriteLine(heroThree.heroThreeName + "'s health is " + heroThree.health);
+		Console.WriteLine(heroThree.heroThreeName + "'s power level is " + heroThree.powerLevel);
 	}
 }
 
@@ -57,4 +75,5 @@ public class Hero {
 	public int powerLevel;
 	public string heroOneName = "John Cena";
 	public string heroTwoName = "Lebron James";
+	public string heroThreeName = "Dani Devito";
 }
