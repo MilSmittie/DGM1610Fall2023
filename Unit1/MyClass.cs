c569504bd3ac3db8e4277664eb526197d7d682d2
MilSmittie <122853314+MilSmittie@users.noreply.github.com>
2023-09-07 23:21:29 -0600

    Adding User Input into MyClass

--- Unit1/MyClass.cs
+++ Unit1/MyClass.cs
@@ -1,5 +1,6 @@
-// This program I created doesn't do a ton. All it can do right now is write down-
-// the statistics for each character, but I would love to work to make this more interactive! 
+// This program I created doesn't do a ton. All it can do right now is determine a winner-
+// only based on "swagger percentage". In the future, I would love to work to make this- 
+// more interactive and have the other variables play a role in who wins.
 
 using System;
 
@@ -13,41 +14,61 @@ public class Program
 		rapperOne = new Rapper();
 		rapperTwo = new Rapper();
 		
+		//User Input Code
 		Console.WriteLine("ROUND 1: LIL PATTY VS LIL SPONGE");
 		Console.WriteLine();
+		Console.WriteLine("Enter Lil Patty's swagger percentage:");
+		int rapperOneswagger = Convert.ToInt32(Console.ReadLine());
+		Console.WriteLine("Enter Lil Sponge's swagger percentage:");
+		int rapperTwoswagger = Convert.ToInt32(Console.ReadLine());
 		
-		//Lil Patty's statistics
-		rapperOne.swagger = 100;
+		//Storing Lil Patty's statistics;
 		rapperOne.flow = 72;
 		rapperOne.crowdControl = 8;
 		rapperOne.roast = 203;
 		
-		//Lil Sponge's statistics
+		//Storing Lil Sponge's statistics
 		rapperTwo.swagger = 75;
 		rapperTwo.flow = 65;
 		rapperTwo.crowdControl = 6;
 		rapperTwo.roast = 97;
 		
-		//Writing Lil Patty's statistics
+		//Displaying Lil Patty's statistics
 		
-		Console.WriteLine("Lil Patty's swag is at " + rapperOne.swagger + " percent");
+		Console.WriteLine();
+		Console.WriteLine("Lil Patty's swag is at " + rapperOneswagger + " percent");
 		Console.WriteLine("Lil Patty is flowin at a rate of " + rapperOne.flow + "mph");
 		Console.WriteLine("Lil Patty has the croud roarin at a solid " + rapperOne.crowdControl + " out of 10");
 		Console.WriteLine("Lil Patty's roasts are a whoppin " + rapperOne.roast + " degrees");
 		Console.WriteLine();
 		
-		//Writing Lil Sponge's statistics
+		//Displaying Lil Sponge's statistics
 		
-		Console.WriteLine("Lil Sponge's swag is at " + rapperTwo.swagger + " percent");
+		Console.WriteLine("Lil Sponge's swag is at " + rapperTwoswagger + " percent");
 		Console.WriteLine("Lil Sponge is flowin at a rate of " + rapperTwo.flow + "mph");
 		Console.WriteLine("Lil Sponge has the croud roarin at a solid " + rapperTwo.crowdControl + " out of 10");
 		Console.WriteLine("Lil Sponge's roasts are a whoppin " + rapperTwo.roast + " degrees");
 		Console.WriteLine();
 		
+		//Determining the winner trough Boolean
+		if (rapperOneswagger > rapperTwoswagger)
+		{
+			Console.WriteLine("LIL PATTY WINS!!!");
+		}
+		else if (rapperTwoswagger > rapperOneswagger)
+		{
+			Console.WriteLine("LIL SPONGE WINS!!!");
+		}
+		else
+			
+		{
+			Console.WriteLine("It's a tie!");
+		}
 		
 	}
 }
 
+//Storing Rapper class variables
 public class Rapper
 {
 	public string rapperOneName = "Lil Patty";
