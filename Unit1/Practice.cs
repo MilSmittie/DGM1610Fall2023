052dc7f11733bd6faec9a4c48d4278a53aa5acb7
MilSmittie <122853314+MilSmittie@users.noreply.github.com>
2023-09-07 23:43:02 -0600

    Practice

--- Unit1/Practice.cs
+++ Unit1/Practice.cs
@@ -7,3 +7,20 @@ public class ClassExamplePractice
         Console.WriteLine("Class Example");
     }
 }
+
+
+int rapperOneswagger = Convert.ToInt32(Console.ReadLine());
+
+if (blankOne > blankTwo)
+		{
+			Console.WriteLine("blankOne WINS!!!");
+		}
+		else if (blankTwo > blankOne)
+		{
+			Console.WriteLine("blankTwo WINS!!!");
+		}
+		else
+			
+		{
+			Console.WriteLine("It's a tie!");
+		}
