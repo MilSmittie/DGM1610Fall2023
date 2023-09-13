ef34e8004de57259f7d3b399211b9b518694d36f
MilSmittie <122853314+MilSmittie@users.noreply.github.com>
2023-09-12 22:32:24 -0600

    Finished Tempurature Advisor

---
+++ Unit1/Unit1bLab/TempuratureAdvisor.cs
@@ -0,0 +1,23 @@
+using System;
+					
+public class Program
+{
+	public void Main()
+	{
+		Console.WriteLine("Temperature Adviser");
+		Console.WriteLine("Enter the tempurature in Celcius:");
+		int value = Convert.ToInt32(Console.ReadLine());
+		Check(value);
+	}
+	
+	public void Check(int value)
+	{
+		if(value > 30)
+		{
+			Console.WriteLine("Stay hydrated and avoid staying in the sun for too long!");
+		} else
+		{
+			Console.WriteLine("Enjoy the pleasant weather!");
+		}
+	}
+}
