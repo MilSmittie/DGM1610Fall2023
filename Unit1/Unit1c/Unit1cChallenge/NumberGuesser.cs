f1ac77f5c6dec5494c705e54a27adc80c7fcb1c4
MilSmittie <122853314+MilSmittie@users.noreply.github.com>
2023-09-20 18:53:05 -0600

    Favorite Food and adding comments

--- Unit1/Unit1c/Unit1cChallenge/NumberGuesser.cs
+++ Unit1/Unit1c/Unit1cChallenge/NumberGuesser.cs
@@ -4,13 +4,18 @@ public class Program
 {
     public static void Main()
     {
+        //Randomizing number program picks
         Random random = new Random();
         int secretNumber = random.Next(1, 11);
+        
+        //Delclaring variables
         int attempts = 1;
         int guess;
 
+        //Displaying instructions
         Console.WriteLine("I'm thinking of a number between 1 and 10. Try to guess it!");
 		
+        //Code to compare user's number to the number the computer picked
         do
         {
 			Console.WriteLine();
@@ -22,22 +27,26 @@ public class Program
 				
                 if (guess < secretNumber)
                 {
-					Console.WriteLine();
+					//If they guessed too low
+                    Console.WriteLine();
 					Console.WriteLine("It's too low! Guess again!");
                 }
                 else if (guess > secretNumber)
                 {
-					Console.WriteLine();
+					//If they guessed too high
+                    Console.WriteLine();
 					Console.WriteLine("It's too high! Guess again!");
                 }
                 else
                 {
-					Console.WriteLine();
+					//If they got it right it displays the programs number and the amount of attempts it took to guess it
+                    Console.WriteLine();
 					Console.WriteLine("Congratulations! You guessed the number " + secretNumber + " in " + attempts + " attempt(s).");
                 }
             }
             else
             {
+                //in case they enter something other than a number between 1 and 10
                 Console.WriteLine();
 				Console.WriteLine("Invalid input. Please enter a valid number between 1 and 10.");
             }
@@ -45,6 +54,7 @@ public class Program
 		
 		while (guess != secretNumber);
 
+        //Ending the game
         Console.WriteLine();
 		Console.WriteLine("Thanks for playing!");
     }
