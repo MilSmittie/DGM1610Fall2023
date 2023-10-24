cdb901023db5ad0d374119ce2e6a780cd752db1e
Amelia Smith <milly.smittie@gmail.com>
2023-10-23 22:35:47 -0600

    Scriptable Objects Lab

---
+++ PracticeScriptableObjects/Assets/Scripts/TextLabelBehavior.cs
@@ -0,0 +1,22 @@
+using System.Globalization;
+using UnityEngine;
+using TMPro;
+
+[RequireComponent(typeof(TextMeshProUGUI))]
+
+public class TextLableBehavior : MonoBehaviour
+{
+    public TextMeshProUGUI label;
+    public FloatData dataObj;
+
+    private void Start()
+    {
+        label = GetComponent<TextMeshProUGUI>();
+        UpdateLabel();
+    }
+
+    public void UpdateLabel()
+    {
+        label.text = dataObj.value.ToString(CultureInfo.InvariantCulture);
+    }
+}
