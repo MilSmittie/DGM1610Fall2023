d4392a6c372e5be2e9723fb6c571757f20006187
Amelia Smith <milly.smittie@gmail.com>
2023-11-29 23:28:55 -0700

    Color Matching Game start

---
+++ Color Matching Game/Assets/Scripts/TextLabelBehaviour.cs
@@ -0,0 +1,30 @@
+using System.Globalization;
+using UnityEngine;
+using TMPro;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(TextMeshProUGUI))]
+
+public class TextLableBehavior : MonoBehaviour
+{
+    private TextMeshProUGUI label;
+    public UnityEvent startEvent;
+
+
+    private void Start()
+    {
+        label = GetComponent<TextMeshProUGUI>();
+        startEvent.Invoke();
+    }
+
+    public void UpdateLabel(FloatData obj)
+    {
+        label.text = obj.value.ToString(CultureInfo.InvariantCulture);
+    }
+
+    public void UpdateLabel(IntData obj)
+    {
+        label.text = obj.value.ToString(CultureInfo.InvariantCulture);
+    }
+}
+
